bad97ec5855f270629aa6321988a325c1a735e63
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2026-03-04 18:20:09 +0530

    Add files via upload

---
+++ contains.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ public class contains{
+    public static void main (String[] args){
+        List <String> names = Arrays.asList("rahul","eva","usmanmurtza","heena","fatima");
+        if(names.contains("rahul")){
+            System.out.println("Match found");
+
+        }
+        else{
+            System.out.println("match not found");
+        }
+    }
+}
+
