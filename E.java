94b94244928840a0ab8ae97537dd9a0eacb99660
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2026-03-16 20:22:51 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,18 @@
+ class Employee{
+    String name;
+    String Desigination;
+    int salary;
+ 
+ Employee(String n,String d,int s){
+    name=n;
+    Desigination=d;
+    salary=s;
+ }
+ void display(){
+    System.out.println("name"+name);
+    System.out.println("Desigination"+Desigination);
+    System.out.println("salary"+salary);
+ }}
+
+ public class Employee
+
