97d213fbc683ffa5017eb7c73188bc26585f8f27
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2026-03-24 23:04:12 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,30 @@
+class person{
+     String name;
+     int age;
+      person(){
+        name= "n";
+        age=1;
+      }
+      }
+    class Student extends person{
+      int rollNo;
+      String course;
+       Student(){
+        super();
+          rollNo=1;
+          course="java";
+       }
+          void show(){
+            System.out.println("rollno :"+rollNo);
+            System.out.println("course:"+course);
+            System.out.println("name:"+name);
+            System.out.println("age:"+age);
+          }
+       
+    }
+
+    public class p1{
+
+     C
+
+
